989c4313811dbe96227a84622b7e82c0d280959a
Vivek <vivekbhardwaj0067@gmail.com>
2020-09-18 16:00:09 +0530

    Comments in template

--- Round 1/template.cpp
+++ Round 1/template.cpp
@@ -1,12 +1,19 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define int long long
+/* 
+** Author : Vivek Bhardwaj
+*/
 
+#include<bits/stdc++.h> //Includes all the modules like vector, set etc from c++ standard library
+using namespace std;    //so that we need not to write std::cout , std::cin ,std::vector<int> while using standard library
+#define int long long   //simple macro so that my int behaves as long long everytime , so that i don't have to type long long everytime 
+
+//int32_t because int means long long , and long long main throws compilation error
+//int32_t means 32 bit integer which is basically int
 int32_t main(){
 
+    //single line for fast input output operations so as to avoid unnecessary TLE
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 
-    
+
 
 
     return 0;
